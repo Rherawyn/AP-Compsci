1dfa563f02e6fa71b5f0d6303132fc42ccf61d5c
Rherawyn <gabrielaltshuler@gmail.com>
2025-10-02 13:11:20 -0700

    Add files via upload

---
+++ Chapter 4 exercises/multadd.java
@@ -0,0 +1,11 @@
+public class multadd {
+    
+    public static void main() {
+        System.out.print(multadd(5,2,5));
+    }
+    
+    public static double multadd(double a, double b, double c) {
+        return(a*b+c);
+        
+    }
+}
