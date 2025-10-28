82846c828a537564471b41c9ea8f75ffde8cc602
Rherawyn <gabrielaltshuler@gmail.com>
2025-10-28 13:00:10 -0700

    Add files via upload

--- chapter 6/Binary.java
+++ chapter 6/Binary.java
@@ -3,11 +3,23 @@
 class Binary {
     
     public static void main (String[] args) {
+        int number = 6;
+        System.out.print(convertToBinary(number));
         
     }
     
     public static String convertToBinary (int num) {
+        String newnum = "";
+        String idiot = "";
+        for(int i = 0; num != 0; i++) {
+            newnum += String.valueOf(num%2);
+            num /= 2;
+        }
         
-        return "funy";
+        for(int i = 0; i < newnum.length(); i++) {
+            idiot += newnum.substring(newnum.length()-1-i,newnum.length()-i);
+        }
+        
+        return idiot;
     }
 }
