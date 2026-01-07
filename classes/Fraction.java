c6268b980c1156c29251662416ed13a988468fd4
Rherawyn <gabrielaltshuler@gmail.com>
2026-01-07 14:58:26 -0800

    Add files via upload

---
+++ classes/Fraction.java
@@ -0,0 +1,45 @@
+
+
+public class Fraction {
+    
+    //instance variables
+    private int numerator;
+    private int denomator;
+    
+    //constructore(s)
+    public Fraction() {
+        this.numerator = 0;
+        this.denomator = 1;
+    }
+    
+    public Fraction(int num, int den) {
+        numerator = num;
+        if (den != 0) {
+            denomator = den;
+        } else {
+            this.denomator = 1;
+            System.out.println("EROROR: DENOMOMATOR CANT BE 000000 IDITI >:((((((((");
+        }
+    }
+    
+    public Fraction(String str) {
+        String[] numbers = str.split("/");
+        int n = Integer.parseInt(numbers[0]);
+        int d = Integer.parseInt(numbers[1]);
+        this.numerator = n;
+        if (d != 0) {
+            denomator = d;
+        } else {
+            this.denomator = 1;
+            System.out.println("EROROR: DENOMOMATOR CANT BE 000000 IDITI >:((((((((");
+        }
+    }
+    
+    public Fraction (Fraction f) {
+        this.numerator = f.numerator;
+        this.denomator = f.denomator;
+    }
+    
+    //behaviour
+    
+}
