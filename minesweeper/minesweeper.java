a34a9b247eb2a82cd243fbf9077f0574d49f5e31
Rherawyn <gabrielaltshuler@gmail.com>
2026-01-19 14:55:49 -0800

    Add files via upload

--- minesweeper/minesweeper.java
+++ minesweeper/minesweeper.java
@@ -9,14 +9,11 @@ class minesweeper {
     final static int EASY = 10;
     final static int MED = 20;
     final static int HARD = 40;
-    static int difficulty = 0;
     
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("input difficulty:");
-        difficulty = in.nextInt();
-        System.out.println(difficulty);
+        int difficulty = in.nextInt();
         mineField = new int[difficulty][difficulty];
         revealed = new boolean [difficulty][difficulty];
         answerKey(difficulty);
@@ -43,32 +40,35 @@ class minesweeper {
         int x = 0;
         int y = 0;
         Scanner in = new Scanner(System.in);
-        String getInput = in.nextLine();
-        String input[] = getInput.split("/");
-         x = Integer.parseInt(input[0]);
-         y = Integer.parseInt(input[1]);
+        String input = in.nextLine();
         System.out.println("\f");
+        String[] inputs = input.split(",");
+        x = Integer.parseInt(inputs[0])-1;
+        y = Integer.parseInt(inputs[1])-1;
         while(revealed[y][x] == true) {
-            System.out.println("bad input");
-            getInput = in.nextLine();
-            input = getInput.split("/");
-            x = Integer.parseInt(input[0]);
-            y = Integer.parseInt(input[1]);
+            input = in.nextLine();
+            System.out.println("\f");
+            inputs = input.split(",");
+            x = Integer.parseInt(inputs[0])-1;
+            y = Integer.parseInt(inputs[1])-1;
         }
-        if(mineField[x][y] == 1) {
+            
+        if(mineField[x][y] == BOMB) {
             System.out.print("\f");
             System.out.print("YOU LOSE");
         } else {
-            sploosh(x,y);
+            if(countSurrounding(x,y) != 0) {
+                revealed[x][y] = true;
+            } else sploosh(x,y);
         }
-            
+        
         int value = 0;
         for(int r = 0; r < revealed.length; r++) {
             for(int c = 0; c < revealed[0].length; c++) {
                 if(revealed[r][c] == true) {
                     if(mineField[r][c] == 0) {
-                        for(int r2 = r-1; r2 < r+2; r2++) {
-                            for(int c2 = c-1; c2 < c+2; c2++) {
+                        for(int r2 = x-1; r2 < x+1; r2++) {
+                            for(int c2 = y-1; c2 < y+1; c2++) {
                                 value += mineField[r2][c2];
                             }
                         }
@@ -85,21 +85,24 @@ class minesweeper {
     }
     
     //for AOE clears
-    public static void sploosh (int x, int y) {
-        if(x < mineField.length && x > 0 && y < mineField[0].length && y > 0 && mineField[x][y] == 0) {
-            revealed[x][y] = true;
-            sploosh(x, y-1);
-            sploosh(x, y+1);
-            sploosh(x-1, y);
-            sploosh(x+1, y);
-            
-            for(int r = x-1; r < r+2; r++) {
-                for(int c = y-1; c < y+2; c++) {
-                    if(r > -1 && c > -1 && r < difficulty && c < difficulty
-                    && mineField[r][c] > 0) revealed[r][c] = true;
-                }
+    public static void sploosh (int r, int c) {
+        if(r < mineField.length && r > 0 && c < mineField[0].length && c > 0 && mineField[r][c] == 0 && !revealed[r][c]) {
+            revealed[r][c] = true;
+            if (countSurrounding(r, c-1) == 0) sploosh(r, c-1);
+            if (countSurrounding(r, c+1) == 0) sploosh(r, c+1);
+            if (countSurrounding(r-1, c) == 0) sploosh(r-1, c);
+            if (countSurrounding(r+1, c) == 0) sploosh(r+1, c);
+        }
+    }
+    
+    public static int countSurrounding(int x, int y) {
+        int value = 0;
+        for(int r2 = x-1; r2 < x+1; r2++) {
+                for(int c2 = y-1; c2 < y+1; c2++) {
+                value += mineField[r2][c2];
             }
         }
+        return value;
     }
     
     public static void answerKey(int difficulty) {
