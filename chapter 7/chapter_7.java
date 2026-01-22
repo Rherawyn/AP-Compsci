cb345385a6c954bbacb720bba256703aaf1575dd
Rherawyn <gabrielaltshuler@gmail.com>
2026-01-21 23:33:04 -0800

    Add files via upload

--- chapter 7/chapter_7.java
+++ chapter 7/chapter_7.java
@@ -3,9 +3,7 @@ class chapter_7 {
     public static void main (String[] args) {
         int[] cheese = {6,12,8,17,11,2};
         System.out.println(indexOfMax(cheese));
-        for(int i = 0; i < 121; i++) {
-        System.out.println(sieve(121)[i]);
-        }
+        System.out.println(areFactors(5, cheese));
     }
     
     public static int indexOfMax(int[] nums) {
@@ -21,14 +19,66 @@ class chapter_7 {
     }
     
     public static boolean[] sieve(int n) {
-        boolean[] done = new boolean [n];
-        for(int i = 2; i < Math.sqrt(n); i++) {
-            done[i] = true;
-            
-            for(int j = (int)Math.pow(i,2); j < n; j = (int)Math.pow(i,2)+j*i) {
-                done[j] = false;
+        boolean[] sieve = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            sieve[i] = true;
+        }
+        sieve[0] = sieve[1] = false;
+
+        int i = 0;
+        while (i < n) {
+            while (sieve[i] == false) {
+                i++;
+            }
+            int cPrime = i;
+            while (i < n) {
+                i = i + cPrime;                                        
+                if (i < n) 
+                sieve[i] = false;  
+                }
+            i = cPrime+1;
+        }
+        return sieve;
+    }
+    
+    public static boolean areFactors(int n, int[] f) {
+        for(int i = 0; i < f.length; i++) {
+            if(f[i]%n != 0) {
+                return false;
             }
         }
-        return done;
+        return true;
     }
+    
+    public static boolean arePrimeFactors(int n, int[] f) {
+        boolean[] primes = sieve(n);      
+        int product = 1;
+        for (int i = 0; i < primes.length; i++) {
+            if (!primes[i]) return false;  
+            product *= i;              
+        }
+        return product == n;  
+    }
+    
+    public static int[] letterHist (String text) {
+        int[] counts = new int[26];   
+        text = text.toLowerCase();    
+        
+        for (int i = 0; i < text.length(); i++) { 
+            char letter = text.charAt(i);   
+            int index = letter - 'a';       
+            counts[index]++;    
+        }
+        return counts;   
+    }
+    
+    public static boolean isAnagram (String a, String b) {
+        int[] aHist = letterHist(a.toLowerCase());    
+        int[] bHist = letterHist(b.toLowerCase());    
+        for (int i = 0; i < aHist.length; i++) {
+            if (aHist[i] != bHist[i]) return false;
+        }
+        return true;                                  
+    }
+    
 }
